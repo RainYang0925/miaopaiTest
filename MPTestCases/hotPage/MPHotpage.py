8d484302162889f9d3c752139aa58f98fca9ac86
liyuanhong <908963295@qq.com>
2016-09-26 17:29:23 +0800

    添加了首页热门的测试用例

---
+++ MPTestCases/hotPage/MPHotpage.py
@@ -0,0 +1,89 @@
+#coding:utf-8
+#Edit by liyuanhong 2016/4/12#
+import unittest
+import Params
+from appium import webdriver
+from time import sleep
+
+class MPHotpage(unittest.TestCase):
+	def __init__(self,methodName):
+		unittest.TestCase.__init__(self, methodName)
+		
+	def init_case(self):
+		#处理开屏广告是否存在的情况
+		try:
+			sleep(1)
+			el = self.driver.find_element_by_id('com.yixia.videoeditor:id/adver_imageview')   #获取开屏广告是否存在
+			self.driver.find_element_by_id('com.yixia.videoeditor:id/textview')    #点击开屏广告上的'点击跳过'按钮
+			sleep(2)
+		except Exception,ex:
+			pass
+			
+	def setUp(self):
+		desired_caps={}
+		desired_caps['device']='android'
+		desired_caps['platformName']='Android'
+		desired_caps['browserName']=''
+		desired_caps['version']='4.4.2'
+		desired_caps['deviceName']='HUAWEI H60-L01'
+
+		#desired_caps['app'] = PATH('D:\\AndroidAutomation\\AndroidAutoTest\\app\\zhongchou.apk')
+		#被测试的App在电脑上的位置
+		desired_caps['appPackage']='com.yixia.videoeditor'
+		desired_caps['appActivity']='.ui.login.SplashActivity'
+		self.driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
+
+	def tearDown(self):
+		self.driver.quit();
+		print 'end ... '
+	
+	def click_shouye_rebang_faxian_wo(self):
+		'''按钮的点击测试
+		1、分别对底导上的首页、热榜、发现，我进行了点击
+		'''
+		print 'start click_shouye_rebang_faxian_wo test ...  '
+		self.init_case()
+		sleep(5)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/bottom_feed').click() #点击底导上的首页
+		sleep(2)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/bottom_friend').click() #点击底导上的热榜
+		sleep(2)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/bottom_message_tip').click() #点击底导上的发现
+		sleep(2)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/bottom_my').click() #点击底导的我
+		sleep(2)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/bottom_feed').click() #点击底导上的首页
+		sleep(2)
+		
+	def switch_category_at_shouye_by_click(self):
+		'''按钮的点击测试
+		1、切换完首页所有的分类
+		'''
+		print 'start switch_category_at_shouye_by_click test ...  '
+		self.init_case()
+		sleep(5)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/bottom_feed').click() #点击底导上的首页
+		
+	def switch_category_at_shouye_by_slide(self):
+		'''切换分类页面
+		1、滑动切换完首页所有的分类
+		'''
+		print 'start switch_category_at_shouye_by_slide test ...  '
+		self.init_case()
+		sleep(5)
+		for i in range(0,20):
+			self.driver.swipe(700, 1000, 50, 1000) #向右滑动
+			sleep(2)
+		for i in range(0,20):
+			self.driver.swipe(50, 1000, 700, 1000) #向左滑动
+			sleep(2)
+	
+		
+		
+		
+def suite(self):
+	suite = unittest.TestSuite()  
+	#suite.addTest(MPHotpage('click_shouye_rebang_faxian_wo'))
+	suite.addTest(MPHotpage('switch_category_at_shouye_by_slide'))
+	runner = unittest.TextTestRunner()  
+	runner.run(suite)
--- startTest.py
+++ startTest.py
@@ -8,15 +8,18 @@ sys.path.append(curDir + '\\MPTestCases\\login')
 sys.path.append(curDir + '\\MPTestCases\\shoot')
 sys.path.append(curDir + '\\MPTestCases\\settingPage')
 sys.path.append(curDir + '\\MPTestCases\\test')
+sys.path.append(curDir + '\\MPTestCases\\hotPage')
 
 
 import MPlogin
 import MPshoot
 import MPsetting
 import MPTest
+import MPHotpage
 
 
-MPlogin.suite("0")
-MPshoot.suite("0")
+#MPlogin.suite("0")
+#MPshoot.suite("0")
 #MPTest.suite("0")
-MPsetting.suite("0")
+#MPsetting.suite("0")
+MPHotpage.suite("0")
