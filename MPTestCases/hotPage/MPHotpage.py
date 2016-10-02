d3f9789d3dcd57168c01d426eaa1eff263acc38b
liyuanhong <908963295@qq.com>
2016-10-02 08:45:39 +0800

    modify

--- MPTestCases/hotPage/MPHotpage.py
+++ MPTestCases/hotPage/MPHotpage.py
@@ -63,7 +63,7 @@ class MPHotpage(unittest.TestCase):
 		print 'start switch_category_at_shouye_by_click test ...  '
 		self.init_case()
 		sleep(5)
-		ele1 = self.driver.find_elements_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.RadioButton') #点击底导上的首页
+		ele1 = self.driver.find_elements_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.HorizontalScrollView/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.RadioButton') #获取首页所有的二级分类
 		ele1[0].click()
 		sleep(2)
 		ele1[1].click()
@@ -76,6 +76,12 @@ class MPHotpage(unittest.TestCase):
 		ele1[4].click()
 		sleep(2)
 		ele1[5].click()
+		sleep(2)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/more').click() #点击展开频道更多
+		sleep(2)
+		self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.ImageView').click() #点击关闭全部平道面板
+		sleep(2)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/more').click() #点击展开频道更多
 		sleep(2)
 
 	def switch_Opencategory_at_shouye_by_click(self):
@@ -101,6 +107,10 @@ class MPHotpage(unittest.TestCase):
 		sleep(1)
 		ele[len(ele) - 2].click()
 		sleep(1)
+		self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.ImageView').click() #点击关闭全部平道面板
+		sleep(2)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/more').click() #点击展开频道更多
+		sleep(2)
 
 
 	def drag_to_change_category_order(self):
@@ -113,11 +123,21 @@ class MPHotpage(unittest.TestCase):
 		self.driver.find_element_by_id('com.yixia.videoeditor:id/more').click() #点击展开频道更多
 		sleep(1)
 		ele = self.driver.find_elements_by_id('com.yixia.videoeditor:id/icon')
-		for i in range(0,2):
-			self.driver.drag_and_drop(ele[2],ele[4])
-			sleep(1)
+		self.driver.drag_and_drop(ele[6],ele[8])
 		sleep(2)
-
+		self.driver.drag_and_drop(ele[4],ele[2])
+		sleep(2)
+		self.driver.drag_and_drop(ele[8],ele[2])
+		sleep(2)
+		self.driver.drag_and_drop(ele[4],ele[10])
+		sleep(2)
+		self.driver.drag_and_drop(ele[10],ele[1])
+		sleep(2)
+		self.driver.find_element_by_xpath('//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.ImageView').click() #点击关闭全部平道面板
+		sleep(2)
+		self.driver.find_element_by_id('com.yixia.videoeditor:id/more').click() #点击展开频道更多
+		sleep(2)
+		
 		
 	def switch_category_at_shouye_by_slide(self):
 		'''切换分类页面
@@ -127,10 +147,10 @@ class MPHotpage(unittest.TestCase):
 		self.init_case()
 		sleep(5)
 		for i in range(0,20):
-			self.driver.swipe(700, 1000, 50, 1000) #向右滑动
+			self.driver.swipe(700, 900, 50, 900) #向右滑动
 			sleep(2)
 		for i in range(0,20):
-			self.driver.swipe(50, 1000, 700, 1000) #向左滑动
+			self.driver.swipe(50, 900, 700, 900) #向左滑动
 			sleep(2)
 	
 		
@@ -138,10 +158,10 @@ class MPHotpage(unittest.TestCase):
 		
 def suite(self):
 	suite = unittest.TestSuite()  
-	#suite.addTest(MPHotpage('click_shouye_rebang_faxian_wo'))
-	#suite.addTest(MPHotpage('switch_category_at_shouye_by_slide'))
-	#suite.addTest(MPHotpage('switch_category_at_shouye_by_click'))
-	#suite.addTest(MPHotpage('switch_Opencategory_at_shouye_by_click'))
+	suite.addTest(MPHotpage('click_shouye_rebang_faxian_wo'))
+	suite.addTest(MPHotpage('switch_category_at_shouye_by_slide'))
+	suite.addTest(MPHotpage('switch_category_at_shouye_by_click'))
+	suite.addTest(MPHotpage('switch_Opencategory_at_shouye_by_click'))
 	suite.addTest(MPHotpage('drag_to_change_category_order'))
 	runner = unittest.TextTestRunner()  
 	runner.run(suite)
