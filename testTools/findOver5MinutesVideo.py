04b450f200ce2e7409e22ea4236caca94c123ea0
liyuanhong <908963295@qq.com>
2016-10-10 14:07:42 +0800

    增加了个人页面的视频时长抓取

--- testTools/findOver5MinutesVideo.py
+++ testTools/findOver5MinutesVideo.py
@@ -6,21 +6,34 @@ sys.setdefaultencoding('gbk')
 import urllib
 import json
 from time import sleep
+import types
+import time
+
+#根据suid获取用户名
+def getUserName(suid):
+	userName = ""
+	#params = urllib.urlencode({'token': 'u~p36u0UjaIvJwxYJpp1wkGdbdeX7LbL','suid':'Z1fV~4uV6WRqfs3xndogdA__','version': '6.6.0.1','per':20})
+	params = urllib.urlencode({'token': 'u~p36u0UjaIvJwxYJpp1wkGdbdeX7LbL','suid':suid,'version': '6.6.0.1','per':20})
+	dat = urllib.urlopen("http://api.miaopai.com/m/space_user_info.json?%s" % params)
+	jdat = json.loads(dat.read())
+	userName = jdat['result']['header']['nick']
+	#print userName
+	return userName
 
 
 #找首页频道超过5分钟的视频
 def testFenlei():
 	categorys = [124,128]
 	theMax = 0
+	thecount = 1
 	for cat in categorys:
-		print "-------------------------------------------------------------"
 		for var in range(1,4):
-			#首页热门
+			#首页各个频道
 			params = urllib.urlencode({'cateid': cat, 'token': 'OIynN5UcxhH082kWvIw7YWP4CTHYSeKI', 'version': '6.6.0.1','page': var,'per':20})
 			dat = urllib.urlopen("http://api.miaopai.com/m/cate2_channel.json?%s" % params)
 			jdat = json.loads(dat.read())
 			it = jdat['per']
-			for i in range(0,it - 1):
+			for i in range(0,it):
 				judge = jdat['result'][i].has_key('color')
 				if judge:
 					pass
@@ -30,7 +43,7 @@ def testFenlei():
 					if theTime > theMax:
 						theMax = theTime
 					if theTime >= 300:
-						print "channel : " + jdat['name']
+						print "channel : " + jdat['name'] + "第 " + str(var) + "页"
 						print "时长 : " + str(theTime) + " 秒"
 						print "url : " + jdat['result'][i]['channel']['stream']['base'] + ".mp4"
 						print "time : " + jdat['result'][i]['channel']['ext']['finishTimeNice']
@@ -39,6 +52,9 @@ def testFenlei():
 						print "pic : " + jdat['result'][i]['channel']['pic']['base'] + ".jpg"
 						print "***********************************************"
 						print ""
+					else:
+						print str(thecount) + "  --  this video is less than 300 seconds !"
+					thecount = thecount + 1
 			sleep(2)
 	print ""
 	print ""
@@ -50,13 +66,14 @@ def testFenlei():
 #找热门超过5分钟的视频
 def testHot():
 	theMax = 0
+	thecount = 1
 	for var in range(1,10):
 		#首页热门
 		params = urllib.urlencode({'token': 'OIynN5UcxhH082kWvIw7YWP4CTHYSeKI', 'version': '6.6.0.1','page': var,'per':20})
 		dat = urllib.urlopen("http://api.miaopai.com/m/v6_hot_channel.json?%s" % params)
 		jdat = json.loads(dat.read())
 		it = jdat['per']
-		for i in range(0,it - 1):
+		for i in range(0,it):
 			judge = jdat['result'][i].has_key('color')
 			if judge:
 				pass
@@ -66,7 +83,7 @@ def testHot():
 				if theTime > theMax:
 					theMax = theTime
 				if theTime >= 300:
-					print "channel : " + "热门"
+					print "channel : " + "热门" + "第 " + str(var) + "页"
 					print "时长 : " + str(theTime) + " 秒"
 					print "url : " + jdat['result'][i]['channel']['stream']['base'] + ".mp4"
 					print "time : " + jdat['result'][i]['channel']['ext']['finishTimeNice']
@@ -75,6 +92,9 @@ def testHot():
 					print "pic : " + jdat['result'][i]['channel']['pic']['base'] + ".jpg"
 					print "***********************************************"
 					print ""
+				else:
+					print str(thecount) + "  --  this video is less than 300 seconds !"
+				thecount = thecount + 1
 		sleep(2)
 	print ""
 	print ""
@@ -83,11 +103,88 @@ def testHot():
 	print theMax
 
 
-testFenlei()
-#testHot()
+#找个人主页超过5分钟的视频
+def testMyPage(suid):
+	theMax = 0
+	timeflag = 0
+	page = 1
+	thecount = 1
+	while(int(timeflag) != -1):
+		#print timeflag
+		#params = urllib.urlencode({'token': 'u~p36u0UjaIvJwxYJpp1wkGdbdeX7LbL','suid':'Z1fV~4uV6WRqfs3xndogdA__','version': '6.6.0.1','timeflag':timeflag,'per':20})
+		params = urllib.urlencode({'token': 'u~p36u0UjaIvJwxYJpp1wkGdbdeX7LbL','suid':suid,'version': '6.6.0.1','timeflag':timeflag,'per':20})
+		dat = urllib.urlopen("http://api.miaopai.com/m/channel_forward_reward.json?%s" % params)
+		jdat = json.loads(dat.read())
+		timeflag = jdat['result']['timeflag']
+		it = jdat['result']['stream']['cnt']
+
+		it = int(jdat['result']['stream']['cnt'])
+		for i in range(0,it):
+			judge = jdat['result']['stream']['list'][i].has_key('channel')
+			#print judge
+			if not judge:
+				pass
+			else:
+				theTime = jdat['result']['stream']['list'][i]['channel']['ext']['length']
+				#print theTime
+				if theTime > theMax:
+					theMax = theTime
+				if theTime >= 300:
+					print getUserName(suid) + "第 " + str(page) + "页"
+					print 'timeFlag : ' + str(jdat['result']['timeflag'])
+					print "时长 : " + str(theTime) + " 秒"
+					print "url : " + jdat['result']['stream']['list'][i]['channel']['stream']['base'] + ".mp4"
+					print "time : " + jdat['result']['stream']['list'][i]['channel']['ext']['finishTimeNice']
+					#x = time.localtime(jdat['result']['stream']['list'][i]['channel']['ext']['finishTime'])
+					#time.strftime('%Y-%m-%d %H:%M:%S',x)
+					print "t : " + jdat['result']['stream']['list'][i]['channel']['ext']['t']
+					print "ft : " + jdat['result']['stream']['list'][i]['channel']['ext']['ft']
+					print "pic : " + jdat['result']['stream']['list'][i]['channel']['pic']['base'] + ".jpg"
+					print "***********************************************"
+					print ""
+				else:
+					print str(thecount) + "  --  this video is less than 300 seconds !"
+				thecount = thecount + 1
+		page = page + 1
+		sleep(2)
+	print ""
+	print ""
+	print ""
+	print "the max is : "
+	print theMax
+
+
+
+
+
+
+
+
+
+
+
+
+#testFenlei()
+testHot()
+
+#suid = 'Z1fV~4uV6WRqfs3xndogdA__'   #凡益网趣的suid
+#suid = 'SBr3gTpwGbI53Lhyuz4xCg__'   #yiixia24000v的suid
+#suid = 'AkwnpO4BXM4~G5BN' #远洪88的suid
+#testMyPage(suid)
+
+#getUserName('Z1fV~4uV6WRqfs3xndogdA__')
+
+
+
+
 
 
 
 
 #http://api.miaopai.com/m/v6_hot_channel.json?token=OIynN5UcxhH082kWvIw7YWP4CTHYSeKI&refresh=3&version=6.6.0.1&page=2&per=20   #热门接口
 #http://api.miaopai.com/m/cate2_channel.json?cateid=128&token=OIynN5UcxhH082kWvIw7YWP4CTHYSeKI&version=6.6.0.1&page=1&per=20   #频道接口
+#http://api.miaopai.com/m/channel_forward_reward.json?token=u~p36u0UjaIvJwxYJpp1wkGdbdeX7LbL&suid=Z1fV~4uV6WRqfs3xndogdA__&version=6.6.0.1&timeflag=1475982730748&per=20 #个人主页
+#http://api.miaopai.com/m/space_user_info.json?token=u~p36u0UjaIvJwxYJpp1wkGdbdeX7LbL&version=6.6.0.1&suid=Z1fV~4uV6WRqfs3xndogdA__  #个人页面个人信息
+
+
+
